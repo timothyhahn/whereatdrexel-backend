fefcd30762bf047d64c46e79ce393f88cfe9875a
Timothy Hahn <timyhahn@gmail.com>
2013-09-23 13:09:24 -0400

    (FEATURE) Admin login page

--- whereatdrexel/models.py
+++ whereatdrexel/models.py
@@ -74,4 +74,18 @@ class User(Model):
 
     def __repr__(self):
         return '<User %r>' % (self.username)
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return unicode(self.id)
+    
+    def is_valid(self, password=None):
+        return bcrypt.check_password_hash(self.password, password)
 
